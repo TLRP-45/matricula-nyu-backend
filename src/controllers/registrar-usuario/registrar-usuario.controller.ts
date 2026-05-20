4a5aaf7c346eb4b0e10f3c4958b39a335434ce6d
GDMiguel <gonzalo.demiguel.castillo@alumnos.uta.cl>
2026-05-19 21:39:31 -0400

    Create registrar-usuario.controller.ts

---
+++ src/controllers/registrar-usuario/registrar-usuario.controller.ts
@@ -0,0 +1,60 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { EstudianteService } from '../../modules/estudiante/estudiante.service';
+
+import {
+  IsEmail,
+  IsString,
+  Length,
+  IsDateString,
+  IsIn
+} from 'class-validator';
+
+class RegistroEstudianteDto {
+
+    @IsString()
+    @Length(2,100)
+    nombre!: string;
+
+    @IsString()
+    @Length(2,100)
+    apellido!: string;
+
+    @IsEmail()
+    email!: string;
+
+    @IsString()
+    rut!: string;
+
+    @IsString()
+    nacionalidad!: string;
+
+    @IsIn(['M','F','O'])
+    sexo!: 'M' | 'F' | 'O';
+
+    @IsString()
+    nacimiento!: string;
+
+    @IsString()
+    direccion!: string;
+
+    @IsString()
+    telefono!: string;
+
+    @IsString()
+    password!: string;
+}
+
+@Controller('registrar-usuario')
+export class RegistrarUsuarioController {
+
+    constructor(
+        private readonly estudianteService: EstudianteService
+    ) {}
+
+    @Post()
+    async registrar(
+        @Body() dto: RegistroEstudianteDto
+    ) {
+        return this.estudianteService.registrar(dto);
+    }
+}
