a0d4189e3a7c7a454618a3aeca5d1f02206fb01e
MaylingAlexia <mayling.alvarez.vargas@alumnos.uta.cl>
2026-05-27 17:26:38 -0400

    Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'reflect-metadata';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
@@ -14,7 +15,16 @@ async function bootstrap() {
         enableImplicitConversion: true,
       }, whitelist: true,
     }
-  ))
+  ));
+
+  const config = new DocumentBuilder()
+    .setTitle('Matricula NYU')
+    .setDescription('Sistema de Matrícula y Gestión Académica NYU')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
