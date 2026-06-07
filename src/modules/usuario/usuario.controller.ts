1acff0c59e3236c5bdede2c1dbecce24b9187a75
GDMiguel <gonzalo.demiguel.castillo@alumnos.uta.cl>
2026-06-07 18:25:26 -0400

    Update usuario.controller.ts

--- src/modules/usuario/usuario.controller.ts
+++ src/modules/usuario/usuario.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller,Get,Param } from '@nestjs/common';
+import { EstudianteService } from './usuario.service';
 
 @Controller('usuario')
-export class UsuarioController {}
+export class UsuarioController {
+
+}
+
+@Controller('estado-academico')
+export class EstadoAcademicoController {
+
+    constructor(
+        private readonly estudianteService: EstudianteService
+    ) {}
+
+    @Get(':id')
+    async obtenerEstado(
+        @Param('id') id: number
+    ) {
+        return this.estudianteService.obtenerEstadoAcademico(id);
+    }
+}
