b6427b0aa0d8441bb9328bff7775813dffbd14c6
MaylingAlexia <mayling.alvarez.vargas@alumnos.uta.cl>
2026-07-05 22:01:13 -0400

    Blolque horario test arreglado.

--- src/test/bloque-horario/bloque-horario.controller.spec.ts
+++ src/test/bloque-horario/bloque-horario.controller.spec.ts
@@ -1,6 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BloqueHorarioController } from '../../modules/bloque-horario/bloque-horario.controller';
-import { BloqueHorarioService } from '../../modules/bloque-horario/bloque-horario.service';
 
 describe('BloqueHorarioController', () => {
   let controller: BloqueHorarioController;
@@ -8,7 +7,6 @@ describe('BloqueHorarioController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BloqueHorarioController],
-      providers: [{provide: BloqueHorarioService, useValue: {}}]
     }).compile();
 
     controller = module.get<BloqueHorarioController>(BloqueHorarioController);
